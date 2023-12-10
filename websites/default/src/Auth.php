320acbea37c67eb09d7b54a3b5b046403df19132
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-12-10 14:22:02 +0000

    ORM updates

---
+++ websites/default/src/Auth.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Trulyao\Eds;
+
+use Trulyao\Eds\Models\User;
+
+final class Auth
+{
+  public static function isLoggedIn(): bool
+  {
+    return isset($_SESSION["user_id"]) || isset($_COOKIE["user_id"]);
+  }
+
+  public static function login(User $user, bool $remember = false): void
+  {
+    $_SESSION["user_id"] = $user->id;
+    if ($remember) {
+      // remember for the next 30 days
+      setcookie("user_id", $user->id, time() + 60 * 60 * 24 * 30);
+    }
+  }
+
+  public static function logout(): void
+  {
+    unset($_SESSION["user_id"]);
+    if (isset($_COOKIE["user_id"])) {
+      setcookie("user_id", "", time() - 3600);
+    }
+  }
+
+  public static function getUser(): ?User
+  {
+    $user_id = self::getLoggedInUserID();
+    return User::findById($user_id);
+  }
+
+  private static function getLoggedInUserID(): int
+  {
+    if (isset($_SESSION["user_id"])) {
+      return (int)$_SESSION["user_id"];
+    } elseif (isset($_COOKIE["user_id"])) {
+      return (int)$_COOKIE["user_id"];
+    }
+    throw new \Exception("User is not logged in");
+  }
+}
--- websites/default/src/Models/BaseModel.php
+++ websites/default/src/Models/BaseModel.php
@@ -22,6 +22,143 @@ abstract class BaseModel
     self::$db = Database::getInstance()->getConnection();
   }
 
+
+
+
+  public static function getTableName(): string
+  {
+    return self::pluralize(strtolower((new \ReflectionClass(static::class))->getShortName()));
+  }
+
+  public static function findById(int $id): ?self
+  {
+    $sql = "SELECT * FROM " . self::getTableName() . " WHERE id = ?";
+    $stmt = self::$db->prepare($sql);
+    $stmt->execute([$id]);
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, static::class);
+    return $stmt->fetch();
+  }
+  /**
+   * @param array<int,mixed> $params
+   */
+  public static function query(string $sql, array $params = []): array
+  {
+    $stmt = self::$db->prepare($sql);
+    $stmt->execute($params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+   * @param       array<int,mixed> $params
+   * @description an escape hatch for executing raw sql queries
+   */
+  public static function execute(string $sql, array $params = []): bool
+  {
+    $stmt = self::$db->prepare($sql);
+    return $stmt->execute($params);
+  }
+
+  public static function all(): array
+  {
+    $sql = "SELECT * FROM " . self::getTableName();
+    $stmt = self::$db->prepare($sql);
+    $stmt->execute();
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, static::class);
+    return $stmt->fetchAll();
+  }
+  /**
+   * @param array<int,mixed> $params
+   */
+  public static function where(string $expr, array $params): array
+  {
+    $sql = "SELECT * FROM " . self::getTableName() . " WHERE {$expr}";
+    $stmt = self::$db->prepare($sql);
+    $stmt->execute($params);
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, static::class);
+    return $stmt->fetchAll();
+  }
+
+
+  public static function lastInsertedId(): int
+  {
+    return self::$db->lastInsertId();
+  }
+
+  public static function count(): int
+  {
+    $stmt = self::$db->prepare("SELECT COUNT(*) FROM " . self::getTableName());
+    $stmt->execute();
+    return $stmt->fetchColumn();
+  }
+
+
+  public function delete(): bool
+  {
+    $sql = "DELETE FROM " . self::getTableName() . " WHERE id = ?";
+    $stmt = self::$db->prepare($sql);
+    return $stmt->execute([$this->{self::getPrimaryKeyColumn()}]);
+  }
+
+  public function save(): bool
+  {
+    $values = array_filter($this->getValues(), fn ($value) => $value !== null);
+    $columns = implode(", ", array_keys($values));
+    $placeholders = implode(", ", array_map(fn ($col) => ":{$col}", array_keys($values)));
+    $sql = "INSERT INTO {$this->getTableName()} ({$columns}) VALUES ({$placeholders})";
+    $stmt = $this->db->prepare($sql);
+    return $stmt->execute($values);
+  }
+
+  /**
+   * @param   array<string>|null $forceUpdate
+   * @example ```
+   * $old = User::findById(1);
+   * $user = new User();
+   * $user->id = $old->id;
+   * $user->username = "new_username";
+   * $user->update();
+   * ```
+   * */
+
+  public function update(?array $forceUpdate = null): bool
+  {
+    $values = [];
+    $columns = [];
+    foreach ($this->getValues() as $key => $value) {
+      // if the key is in the forceUpdate array, add it to the update query even if the value is null
+      if (($value !== null && !in_array($key, $forceUpdate ?? [])) && $key !== self::getPrimaryKeyColumn()) {
+        $values[$key] = $value;
+        $columns[] = "{$key} = :{$key}";
+      } elseif ($key === self::getPrimaryKeyColumn()) {
+        // if the key is the primary key, add it to the values array ONLY because we don't want to update the primary key
+        $values[$key] = $value;
+      }
+    }
+
+    if (empty($columns)) {
+      return false;
+    } elseif (!isset($values[self::getPrimaryKeyColumn()])) {
+      throw new \Exception("Cannot update record without primary key, if the primary key for this model is not `id` override the self::getPrimaryKeyColumn() method");
+    }
+
+    $sql = "UPDATE {$this->getTableName()} SET {$columns} WHERE " . self::getPrimaryKeyColumn() . " = :id";
+    $stmt = $this->db->prepare($sql);
+    return $stmt->execute($values);
+  }
+
+  /**
+   * @description Useful for updating a model with a partial model, for example when updating a user profile and only updating the fields that were changed
+   * */
+  public function merge(self $model): self
+  {
+    foreach ($this->getAttributes() as $attr) {
+      if ($model->{$attr} !== null) {
+        $this->{$attr} = $model->{$attr};
+      }
+    }
+    return $this;
+  }
+
   // restrict setting properties that don't exist and certain internal properties even though they behave like public properties
   public function __set(string $name, mixed $value): void
   {
@@ -54,85 +191,14 @@ abstract class BaseModel
     return $values;
   }
 
-  public static function getTableName(): string
-  {
-    return self::pluralize(strtolower((new \ReflectionClass(static::class))->getShortName()));
-  }
-
-  public static function findById(int $id): ?self
-  {
-    $sql = "SELECT * FROM " . self::getTableName() . " WHERE id = ?";
-    $stmt = self::$db->prepare($sql);
-    $stmt->execute([$id]);
-    $stmt->setFetchMode(\PDO::FETCH_CLASS, static::class);
-    return $stmt->fetch();
-  }
-
-  public function save(): bool
-  {
-    $values = array_filter($this->getValues(), fn ($value) => $value !== null);
-    $columns = implode(", ", array_keys($values));
-    $placeholders = implode(", ", array_map(fn ($col) => ":{$col}", array_keys($values)));
-    $sql = "INSERT INTO {$this->getTableName()} ({$columns}) VALUES ({$placeholders})";
-    $stmt = $this->db->prepare($sql);
-    return $stmt->execute($values);
-  }
-
-  /**
-   * @param   array<string>|null $forceUpdate
-   * @example ```
-   * $old = User::findById(1);
-   * $user = new User();
-   * $user->id = $old->id;
-   * $user->username = "new_username";
-   * $user->update();
-   * ```
-   * */
-
-  public function update(?array $forceUpdate = null): bool
-  {
-    $values = [];
-    $columns = [];
-    foreach ($this->getValues() as $key => $value) {
-      // if the key is in the forceUpdate array, add it to the update query even if the value is null
-      if (($value !== null && !in_array($key, $forceUpdate ?? [])) && $key !== self::getPrimaryKey()) {
-        $values[$key] = $value;
-        $columns[] = "{$key} = :{$key}";
-      } elseif ($key === self::getPrimaryKey()) {
-        // if the key is the primary key, add it to the values array ONLY because we don't want to update the primary key
-        $values[$key] = $value;
-      }
-    }
-
-    if (empty($columns)) {
-      return false;
-    } elseif (!isset($values[self::getPrimaryKey()])) {
-      throw new \Exception("Cannot update record without primary key, if the primary key for this model is not `id` override the self::getPrimaryKey() method");
-    }
-
-    $sql = "UPDATE {$this->getTableName()} SET {$columns} WHERE " . self::getPrimaryKey() . " = :id";
-    $stmt = $this->db->prepare($sql);
-    return $stmt->execute($values);
-  }
-
-  /**
-   * @description Useful for updating a model with a partial model, for example when updating a user profile and only updating the fields that were changed
-   * */
-  public function merge(self $model): self
-  {
-    foreach ($this->getAttributes() as $attr) {
-      if ($model->{$attr} !== null) {
-        $this->{$attr} = $model->{$attr};
-      }
-    }
-    return $this;
-  }
 
 
-  protected static function getPrimaryKey(): string
+  protected static function getPrimaryKeyColumn(): string
   {
     return "id";
   }
+
+
 
   private static function pluralize(string $word): string
   {
